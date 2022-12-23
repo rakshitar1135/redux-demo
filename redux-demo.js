fc7d704f6b43ac2296b62085b668b3238eaf00cc
rakshitar1135 <rakshitaraju2000@gmail.com>
2022-12-23 19:16:00 +0530

    changes

--- redux-demo.js
+++ redux-demo.js
@@ -1,8 +1,15 @@
 const redux=require('redux')
 const counterReducer=(state={counter:0},action)=>{
+    if(action.type==='increment'){
     return{
     counter:state.counter+1
     }
+}
+if(action.type==='decrement'){
+    return{
+    counter:state.counter-1
+    }
+}
 }
 const store=redux.createStore(counterReducer);
 console.log(store.getState());
@@ -12,10 +19,7 @@ console.log(latestState);
 };
 store.subscribe(counterSubscriber)
 store.dispatch({type:'increment'});
-store.dispatch({type:'increment'});
-store.dispatch({type:'increment'});
-store.dispatch({type:'increment'});
-store.dispatch({type:'increment'});
+store.dispatch({type:'decrement'})
 
 
 
